29bf11f78482af3f83b6979fca81825a6916960e
Mizuki Baba <info@lovelovetrb.com>
2026-04-28 16:32:48 +0900

    feat: dockdockgoを用いた検索ツールの実装

---
+++ src/mnemos/tools/__init__.py
@@ -0,0 +1,5 @@
+"""LLMが利用できるツール群を定義するレイヤー"""
+
+from mnemos.tools.search import search_tool
+
+tools = [search_tool]
---
+++ src/mnemos/tools/search.py
@@ -0,0 +1,5 @@
+"""DuckDuckGo検索ツール"""
+
+from langchain_community.tools import DuckDuckGoSearchResults
+
+search_tool = DuckDuckGoSearchResults()
